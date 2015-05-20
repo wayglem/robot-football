e3d0e530fa670f9c78fcd7218d7528f67aec6391
wayglem <elias.salam@gmail.com>
2015-05-20 15:06:22 +0200

    Changed olympic distance. Distance is now pretty accurate...

--- testCapteurs/testCapteurs.ino
+++ testCapteurs/testCapteurs.ino
@@ -17,46 +17,72 @@ void setup() {
 
 void loop() {
   String frontValue = String(Robot.analogRead(frontSensor));
-  
   Robot.clearScreen();
   Robot.stroke(0, 0, 0);
   Robot.textSize(2);
-  Robot.text(getOlympicDistance(frontSensor),54,5);  
-  Robot.text(getOlympicDistance(leftSensor),5,80);  
-  Robot.text(getOlympicDistance(rightSensor),100,80);  
-  Robot.text(getOlympicDistance(rearSensor),54,140);
-  
+  int * distances =getRecurentDistance();
+  Robot.text(distances[1],54,5);  
+  Robot.text(distances[0],5,80);  
+  Robot.text(distances[2],100,80);  
+  Robot.text(distances[3],54,140);
   Robot.text((int)Robot.compassRead(), 54, 80); //affiche la valeur du compas
-  
-  delay(1000);
-}
-
-int getOlympicDistance(int sensorPin) {
-  Serial.print(sensorPin);
-  Serial.print("[");
-  int ITERATIONS = 10;
-  int values[ITERATIONS];
-  int min = 9999;
-  int max = 0;
-  int sum = 0;
-  for (int i=0;i<ITERATIONS;i++){
-    values[i] = Robot.analogRead(sensorPin);
-    Serial.print(values[i]);
-    Serial.print(", ");
-    sum += values[i];
-    if(values[i] < min) {
-      min = values[i];
-    }
-    if(values[i] > max) {
-      max = values[i];
-    }
-     delay(10);
-  }
-  Serial.print("]");
-  Serial.println("");
-  return (int) (((sum - (max + min )) / (ITERATIONS - 4))*1.27);
+  free(distances);
+  delay(3000);
 }
 
 int getDistance(int sensorPin) {
   return (int) Robot.analogRead(sensorPin)*1.27;
+}
+
+int * getOlympicDistance() {
+  int ITERATIONS = 50;
+  int values[4][ITERATIONS];
+  int a[ITERATIONS];
+  int sum = 0;
+  int * sums = new int[4];
+  sums[0]=0;
+  sums[1]=0;
+  sums[2]=0;
+  sums[3]=0;
+  
+  int OLYMPICNBTRUNCATE = 10;
+  Serial.print("avant : [ ");
+  for (int i=0;i<ITERATIONS;i++){
+    values[0][i] = Robot.analogRead(M0);
+    values[1][i] = Robot.analogRead(M2);
+    a[i] = Robot.analogRead(M2);
+    values[2][i] = Robot.analogRead(M4);
+    values[3][i] = Robot.analogRead(M6);
+    sum = sum + values[1][i];
+    Serial.print(values[1][i]);
+    Serial.print(", ");
+    delay(20);
+  }
+  Serial.print("]\n");
+  sort(values[0], ITERATIONS);
+  sort(values[1], ITERATIONS);
+  sort(values[2], ITERATIONS);
+  sort(values[3], ITERATIONS);
+  for (int i=OLYMPICNBTRUNCATE-1;i<ITERATIONS-OLYMPICNBTRUNCATE-1;i++) {
+    sums[0] = sums[0]+values[0][i];
+    sums[1] = sums[1]+values[1][i];
+    sums[2] = sums[2]+values[2][i];
+    sums[3] = sums[3]+values[3][i];
+  }
+  for(int j=0; j<4;j++){
+   sums[j]=(int) (sums[j]/(ITERATIONS-(2*OLYMPICNBTRUNCATE)))*1.27;
+  }
+  Serial.println("");
+  return sums;
+}
+void sort(int a[], int size) {
+    for(int i=0; i<(size-1); i++) {
+        for(int o=0; o<(size-(i+1)); o++) {
+                if(a[o] > a[o+1]) {
+                    int t = a[o];
+                    a[o] = a[o+1];
+                    a[o+1] = t;
+                }
+        }
+    }
 }
