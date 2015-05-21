9623987dbfcca1a1566fa8415792da1c04a2d42e
wayglem <elias.salam@gmail.com>
2015-05-21 17:12:41 +0200

    added function to make the robot move forward until it's at x cm from an obstacle.

---
+++ moveTo/moveTo.ino
@@ -0,0 +1,107 @@
+#include <ArduinoRobot.h>
+#include <SPI.h>
+#include <Wire.h>
+
+int rayonRobot = 11;
+int hPlan = 135;
+int lPlan = 103;
+int ml = 12; // marge largeur
+int mh = 12; // marge hauteur
+int a0 = 90;
+
+void setup() {
+  // put your setup code here, to run once:
+  Robot.begin();
+  Robot.beginTFT();
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  moveForwardUntil(20);
+  Robot.turn(90);
+}
+
+// TODO : a transformer en .h
+// [0] = x2 (droite)
+// [1] = y1 (haut)
+// [2] = x1 (gauche)
+// [3] = y2 (bas)
+int * getOlympicDistance() {
+  int ITERATIONS = 50;
+  int values[4][ITERATIONS];
+  int * sums = new int[4];
+  sums[0] = 0;
+  sums[1] = 0;
+  sums[2] = 0;
+  sums[3] = 0;
+  int OLYMPICNBTRUNCATE = 10;
+  for (int i = 0; i < ITERATIONS; i++) {
+    values[0][i] = Robot.analogRead(M0);
+    values[1][i] = Robot.analogRead(M2);
+    values[2][i] = Robot.analogRead(M4);
+    values[3][i] = Robot.analogRead(M6);
+    delay(20);
+  }
+  sort(values[0], ITERATIONS);
+  sort(values[1], ITERATIONS);
+  sort(values[2], ITERATIONS);
+  sort(values[3], ITERATIONS);
+  for (int i = OLYMPICNBTRUNCATE - 1; i < ITERATIONS - OLYMPICNBTRUNCATE - 1; i++) {
+    sums[0] = sums[0] + values[0][i];
+    sums[1] = sums[1] + values[1][i];
+    sums[2] = sums[2] + values[2][i];
+    sums[3] = sums[3] + values[3][i];
+  }
+  for (int j = 0; j < 4; j++) {
+    sums[j] = (int) (sums[j] / (ITERATIONS - (2 * OLYMPICNBTRUNCATE))) * 1.27;
+  }
+  return sums;
+}
+int * getPosition () {
+  int * distances = getOlympicDistance();
+  int * position = new int [2];
+  // Si le capteur droit est plus proche d'un mur, calculer avec ce capteur
+  if (distances[0] < distances[2]) {
+    position[0] = lPlan + ml - (distances[0] + rayonRobot);
+  }
+  else {
+    position[0] = distances[2] + rayonRobot + ml;
+  }
+  // Si le capteur bas est plus proche d'un mur, calculer avec ce capteur
+  if (distances[3] < distances[1]) {
+    position[1] = hPlan + mh - (distances[3] + rayonRobot);
+  }
+  else {
+    position[1] = distances[1] + rayonRobot + mh;
+  }
+  Robot.text(distances[2] + rayonRobot, 2, 80); //Affiche la distance de gauche
+  Robot.text(distances[1] + rayonRobot, 47, 3); // Affiche la distance du haut
+  Robot.text(distances[0] + rayonRobot, 93, 80); //Affiche la distance de droite
+  Robot.text(distances[3] + rayonRobot, 47, 145); //Affiche la distance du bas
+
+  free(distances);
+  return position;
+}
+void sort(int a[], int size) {
+  for (int i = 0; i < (size - 1); i++) {
+    for (int o = 0; o < (size - (i + 1)); o++) {
+      if (a[o] > a[o + 1]) {
+        int t = a[o];
+        a[o] = a[o + 1];
+        a[o + 1] = t;
+      }
+    }
+  }
+}
+void moveForwardUntil (int distanceFromObstacle){
+  int sensorDistance = (int) Robot.analogRead(M2) * 1.27;
+  if (sensorDistance - distanceFromObstacle < 1) {
+    Robot.motorsStop();
+    return;
+  }
+  Robot.motorsWrite(150,150);
+  delay(20);
+  moveForwardUntil(distanceFromObstacle);
+}
+
