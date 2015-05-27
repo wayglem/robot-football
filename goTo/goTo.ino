66ed24aca5734874e694b12fa7e7eb4aae5fad07
wayglem <elias.salam@gmail.com>
2015-05-27 15:45:23 +0200

    goTo : allow robot to go to a point

--- goTo/goTo.ino
+++ goTo/goTo.ino
@@ -2,10 +2,10 @@
 #include <SPI.h>
 #include <Wire.h>
 
-int haut = 163;
-int gauche = 90;
-int bas = 289;
-int droite = 219;
+int haut = 165;
+int gauche = 95;
+int bas = 291;
+int droite = 221;
 
 int targetX = 64;
 int targetY = 38;
@@ -19,32 +19,21 @@ void setup() {
   // put your setup code here, to run once:
   Robot.begin();
   Robot.beginTFT();
+  Robot.beginSpeaker();
   Robot.stroke(0, 0, 0);
   Robot.textSize(2);
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
-  goToTarget(targetX, targetY);
-}
-void moveUntil(int distanceInCM){
-  int sensorDistance = (int) Robot.analogRead(M2) * 1.27;
-  bool isArrived = false;
-  while(!isArrived) {
-    while(sensorDistance > distanceInCM){
-      Robot.motorsWrite(200,200);
-      delay(10);
-      sensorDistance = (int) Robot.analogRead(M2) * 1.27;
-    }
-    Robot.motorsStop();
-    int * sensorsValues = getOlympicDistance();
-    if (sensorsValues[1] <= distanceInCM) {
-      isArrived = true;
-    }
-    sensorDistance = (int) Robot.analogRead(M2) * 1.27;    
-    free(sensorsValues);
-  }
-  Robot.motorsStop();
+  goToTarget2(targetX, targetY);
+  delay(1000);
+  Robot.beep(BEEP_DOUBLE);
+  delay(5000);
+  goToTarget2(70, 110);
+  delay(1000);
+  Robot.beep(BEEP_DOUBLE);
+  delay(5000);
 }
 // [0] = x2 (droite)
 // [1] = y1 (haut)
@@ -94,36 +83,34 @@ void sort(int a[], int size) {
 }
 
 void goToTarget(int x, int y) {
-  int * position = getPositionField();
-  Robot.clearScreen();
-  if(position[1] < y){
-     Robot.pointTo(bas);
-     
-     Robot.text("Y until:", 5,5);
-     Robot.text(hPlan - y + rayonRobot, 5,50);
-     moveUntil(hPlan - y + rayonRobot); 
-  } else if (position[1] > y) {
-     Robot.pointTo (haut);
-     
-     Robot.text("Y until :", 5,5);
-     Robot.text(y - rayonRobot, 5,50);
-     moveUntil(y - rayonRobot);
-  }
-  delay(1000);
-  if(position[0] > x) {
-     Robot.pointTo(gauche);
-    
-     Robot.text("X until:", 5,5);
-     Robot.text(x - rayonRobot, 5,50);
-      moveUntil(x - rayonRobot);
-  } else if (position[0] < x) {
-     Robot.pointTo(droite);
-     
-     Robot.text("X until:", 5,5);
-     Robot.text(lPlan - x + rayonRobot, 5,50);
-     moveUntil(lPlan - x + rayonRobot); 
-  }
+  int TOLERANCE = 5;
+  while (true){
+    int * position = getPositionField();
+    if ( position[0] >= x - TOLERANCE && position[0] <= x + TOLERANCE
+      && position[1] >= y - TOLERANCE && position[1] <= y + TOLERANCE)
+    {
+        free(position);
+        return;
+    }
+    Robot.clearScreen();
+    if(position[1] < y){
+       Robot.pointTo(bas);
+       moveUntil(hPlan - y - rayonRobot, 1); 
+    } else if (position[1] > y) {
+       Robot.pointTo (haut);     
+       moveUntil(y - rayonRobot, 1);
+    }
+    delay(1000);
+    Robot.clearScreen();
+    if(position[0] > x) {
+       Robot.pointTo(gauche);
+       moveUntil(x - rayonRobot, 0);
+    } else if (position[0] < x) {
+       Robot.pointTo(droite);     
+       moveUntil(lPlan - x - rayonRobot,2);
+    }
   free(position);
+  }
 }
 
 //Retourne un tableau correspondant au coordonnees du centre du robot sur le terrain
@@ -136,7 +123,6 @@ int * getPositionField () {
   Robot.clearScreen();
   if (orientation < (haut + TOLERANCE_BOUSSOLE) % 360 && orientation > (haut - TOLERANCE_BOUSSOLE) % 360){
     // Si le capteur droit est plus proche d'un mur, calculer avec ce capteur
-    Robot.text("haut", 5, 5);
     if (distances[0] < distances[2]) {
       position[0] = lPlan - (distances[0] + rayonRobot);
     }else {
@@ -151,7 +137,6 @@ int * getPositionField () {
   }
   //Si le robot est tourne vers la droite
   else if (orientation < (droite + TOLERANCE_BOUSSOLE) % 360 && orientation > (droite - TOLERANCE_BOUSSOLE) % 360){
-    Robot.text("droite", 5, 5);
     if (distances[1] < distances[3]) {
       position[0] = lPlan - (distances[1] + rayonRobot);
     }else {
@@ -165,7 +150,6 @@ int * getPositionField () {
   }
   //Si le robot est tourne vers la gauche
   else if (orientation < (gauche + TOLERANCE_BOUSSOLE) % 360 && orientation > (gauche - TOLERANCE_BOUSSOLE) % 360){
-    Robot.text("gauche", 5, 5);
     if (distances[1] < distances[3]) {
       position[0] = distances[1] + rayonRobot;
     }else {
@@ -179,7 +163,6 @@ int * getPositionField () {
   }
   //Si le robot est tourne vers le bas
   else if (orientation < (bas + TOLERANCE_BOUSSOLE) % 360 && orientation > (bas - TOLERANCE_BOUSSOLE) % 360){  
-    Robot.text("bas", 5, 5);
     if (distances[0] < distances[2]) {
       position[0] = distances[0] + rayonRobot;
     }else {
@@ -192,11 +175,43 @@ int * getPositionField () {
     }
   }
   else {
-    Robot.text("autre", 5, 5);
-    Robot.text(orientation, 40, 70);
     Robot.pointTo(haut);
     getPositionField();
   }
   free(distances);
   return position;
+}
+
+/**
+  @param orientation: 0 if on x axis; 1 if on y axis.
+*/
+void moveUntil(int distanceInCM, int orientation) {
+  int sensorDistance = (int) Robot.analogRead(M2) * 1.27;
+  int sensorBackDistance = (int) Robot.analogRead(M6) * 1.27;
+  if (sensorBackDistance < sensorDistance) {
+    // ROBOT is on horizontal axis
+    if(orientation = 0) {
+      sensorDistance = lPlan - sensorBackDistance;
+    }
+    // ROBOT is on vertical axis
+    else {
+      sensorDistance = hPlan - sensorBackDistance;      
+    }
+  }
+  bool isArrived = false;
+  while(!isArrived) {
+    while(sensorDistance > distanceInCM){
+      Robot.motorsWrite(200,200);
+      delay(10);
+      sensorDistance = (int) Robot.analogRead(M2) * 1.27;
+    }
+    Robot.motorsStop();
+    int * sensorsValues = getOlympicDistance();
+    if (sensorsValues[1] <= distanceInCM) {
+      isArrived = true;
+    }
+    sensorDistance = (int) Robot.analogRead(M2) * 1.27;    
+    free(sensorsValues);
+  }
+  Robot.motorsStop(); 
 }
